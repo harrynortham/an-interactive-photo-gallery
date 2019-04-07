79472999f0d63c3d588fa52b7fdee5640ed3a67f
Harry Northam <harrynortham@icloud.com>
2019-04-07 15:43:34 +0200

    Fixes to JS

--- js/app.js
+++ js/app.js
@@ -2,23 +2,24 @@
 
 lightbox.option({
 'showImageNumberLabel': false
-})
+});
 
 
 /*Do basic search of images*/
 
 function searchPhotos() {
 
-  var input = document.getElementById('search-gallery');
-  var filter = input.value.toUpperCase();
-  var gallery = document.getElementById("gallery");
+  const input = document.getElementById('search-gallery');
+  const gallery = document.getElementById("gallery");
+  let filter = input.value.toUpperCase();
 
-  var galleryItem = gallery.getElementsByTagName('div');
+  let galleryItem = gallery.getElementsByTagName('div');
 
+  let i;
   for (i = 0; i < galleryItem.length; i++) {
-    a = galleryItem[i].getElementsByTagName("a")[0];
+    let a = galleryItem[i].getElementsByTagName("a")[0];
 
-    txtValue = a.getAttribute("data-title") || a.getAttribute("data-name");
+    let txtValue = a.getAttribute("data-title") || a.getAttribute("data-name");
 
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       galleryItem[i].style.display = "";
